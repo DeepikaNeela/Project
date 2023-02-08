40758de441055bf375386162b9379f5d3c8c18c4
DeepikaNeela <124121736+DeepikaNeela@users.noreply.github.com>
2023-02-08 10:05:44 +0530

    Add files via upload

---
+++ destruct.js
@@ -0,0 +1,25 @@
+//const user={
+    //firstName:"kavya",
+    //lastName:"Sharma",
+    //age:20,
+    //address:{
+        //city:"Mumbai",
+        //country:"India"
+   //}
+//}
+
+//const name=user.firstName;
+//const address = user.address.city;
+
+//const { firstName,lastName,...userInfo}=user;
+//console.log(firstName);
+//console.log(userInfo);
+
+const scores=[10,20,99,334];
+//console.log(scores[0]);
+
+const[x,b,c]=scores;
+console.log(x);
+console.log(b);
+console.log(c);
+//console.log(other)
